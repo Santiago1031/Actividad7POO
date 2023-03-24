388e343659c53b2160a3a1796f2744704f629750
Santiago Cordoba <scordoba067@soyudemedellin.edu.co>
2023-03-23 22:57:21 -0500

    Implement exercise part.2

--- exerciseDunderMethods/exercise.py
+++ exerciseDunderMethods/exercise.py
@@ -8,3 +8,28 @@ class Elemento:
         if isinstance(other, Elemento):
             return self.nombre == other.nombre
         return False
+
+class ListaElementos:
+    def __init__(self):
+        self.elementos = []
+
+    def agregar_elemento(self, elemento: Elemento):
+        self.elementos.append(elemento)
+
+class ConjuntoElementos:
+
+    contador = 0
+    def __init__(self, nombre_conjunto: str):
+        self.nombre = nombre_conjunto
+        self.elementos = []
+        self.__id = type(self).contador
+        type(self).contador += 1
+
+    def agregar_elemento(self, elemento: Elemento):
+        self.elementos.append(elemento)
+
+    @property
+    def id(self):
+        return self.__id
+
+
